233953f7ae7681f2425583edb3d568487ca38059
AdityaDaDDy <adityachd23231@gmail.com>
2025-01-12 17:12:23 +0000

    Pending changes exported from your codespace

---
+++ blast.py
@@ -0,0 +1,39 @@
+ #/$$$$$$$  /$$                     /$$             /$$   /$$           /$$                          
+ #| $$__  $$| $$                    | $$            | $$$ | $$          | $$                          
+# | $$  \ $$| $$  /$$$$$$  /$$$$$$$ | $$   /$$      | $$$$| $$ /$$   /$$| $$   /$$  /$$$$$$   /$$$$$$ 
+ #| $$$$$$$ | $$ |____  $$| $$__  $$| $$  /$$/      | $$ $$ $$| $$  | $$| $$  /$$/ /$$__  $$ /$$__  $$
+# | $$__  $$| $$  /$$$$$$$| $$  \ $$| $$$$$$/       | $$  $$$$| $$  | $$| $$$$$$/ | $$$$$$$$| $$  \__/
+# | $$  \ $$| $$ /$$__  $$| $$  | $$| $$_  $$       | $$\  $$$| $$  | $$| $$_  $$ | $$_____/| $$      
+# $$$$$$$/| $$|  $$$$$$$| $$  | $$| $$ \  $$      | $$ \  $$|  $$$$$$/| $$ \  $$|  $$$$$$$| $$      
+#_______/ |__/ \_______/|__/  |__/|__/  \__/      |__/  \__/ \______/ |__/  \__/ \_______/|__/      
+                                                                                                     
+from http import client
+import threading, webbrowser, discord, random, httpx, json, time, os; from discord.ext import commands , tasks;from itertools import cycle 
+import asyncio
+import itertools
+    
+
+    __intents__ = discord.Intents.default()
+    __intents__.members = True
+    __proxies__, darks, __config__, mohit= cycle(open("proxies.txt", "r").read().splitlines()), commands.Bot(command_prefix="+", intents=__intents__), json.load(open("config.json", "r", encoding="utf-8")), 45
+    os.system("cls") if os.name == "nt" else os.system("clear")
+    token = input("{}({}Blast{}) Enter Token{}:{} ".format("\x1b[0m", "\x1b[38;5;122m", "\x1b[0m", "\x1b[38;5;122m", "\x1b[0m"))
+    guildid = input("{}({}Blast{})Guild ID{}:{} ".format("\x1b[0m", "\x1b[38;5;122m", "\x1b[0m", "\x1b[38;5;122m", "\x1b[0m"))
+    os.system("cls") if os.name == "nt" else os.system("clear")
+
+BLAST ART='''   
+                        adigodplays 
+                        blank nuker mamaa chod de gya
+
+              /$$$$$$$  /$$                     /$$             /$$   /$$           /$$                          
+              | $$__  $$| $$                    | $$            | $$$ | $$          | $$                          
+              | $$  \ $$| $$  /$$$$$$  /$$$$$$$ | $$   /$$      | $$$$| $$ /$$   /$$| $$   /$$  /$$$$$$   /$$$$$$ 
+              | $$$$$$$ | $$ |____  $$| $$__  $$| $$  /$$/      | $$ $$ $$| $$  | $$| $$  /$$/ /$$__  $$ /$$__  $$
+              | $$__  $$| $$  /$$$$$$$| $$  \ $$| $$$$$$/       | $$  $$$$| $$  | $$| $$$$$$/ | $$$$$$$$| $$  \__/
+              | $$  \ $$| $$ /$$__  $$| $$  | $$| $$_  $$       | $$\  $$$| $$  | $$| $$_  $$ | $$_____/| $$      
+              | $$$$$$$/| $$|  $$$$$$$| $$  | $$| $$ \  $$      | $$ \  $$|  $$$$$$/| $$ \  $$|  $$$$$$$| $$      
+              |_______/ |__/ \_______/|__/  |__/|__/  \__/      |__/  \__/ \______/ |__/  \__/ \_______/|__/      
+                                                                                                                  
+
+              
+              
